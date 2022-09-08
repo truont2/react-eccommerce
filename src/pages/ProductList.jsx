ee4adccc10c162a490656aaae876570b4441f85b
Takara Truong <truont2@gmail.com>
2022-09-08 13:21:42 -0700

    created the footer

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ProductList = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default ProductList
